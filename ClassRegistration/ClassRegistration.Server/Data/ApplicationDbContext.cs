17d48be37369e85fb104acb36593af82937843b0
Nguyễn Duy Hào <duyhao301202@gmail.com>
2024-03-25 00:26:09 +0700

    user api test

---
+++ ClassRegistration/ClassRegistration.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ClassRegistration.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassRegistration.Server.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Property(e => e.UserType).HasColumnName("UserType");
+            });
+        }
+    }
+}
--- ClassRegistration/ClassRegistration.Server/Program.cs
+++ ClassRegistration/ClassRegistration.Server/Program.cs
@@ -1,3 +1,8 @@
+using ClassRegistration.Server.Data;
+using ClassRegistration.Server.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace ClassRegistration.Server
 {
@@ -11,10 +16,14 @@ namespace ClassRegistration.Server
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAnyOrigin",
-                builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+                corsBuilder => corsBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<ApplicationUser>().AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             
@@ -23,12 +32,30 @@ namespace ClassRegistration.Server
             app.UseCors("AllowAnyOrigin");
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.MapIdentityApi<ApplicationUser>();
+
+            app.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
+            {
+
+                await signInManager.SignOutAsync();
+                return Results.Ok();
+
+            }).RequireAuthorization();
+
+
+            app.MapGet("/pingauth", (ClaimsPrincipal user) =>
+            {
+                var email = user.FindFirstValue(ClaimTypes.Email); // get the user's email from the claim
+                return Results.Json(new { Email = email }); ; // return the email as a plain text response
+            }).RequireAuthorization();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseMigrationsEndPoint();
                 app.UseSwagger();
                 app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
