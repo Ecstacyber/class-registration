57c5ddcae3ed58d505c4c742994e330219fa6c59
Nguyễn Duy Hào <duyhao301202@gmail.com>
2024-03-26 00:03:05 +0700

    Add Microsoft.AspNetCore.Authentication.JwtBearer NuGet package and database initialiser

---
+++ ClassRegistration/ClassRegistration.Server/Data/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,76 @@
+﻿using ClassRegistration.Server.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace ClassRegistration.Server.Data
+{
+    public class ApplicationDbContextInitialiser(
+        ILogger<ApplicationDbContextInitialiser> logger,
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        private readonly ILogger<ApplicationDbContextInitialiser> _logger = logger;
+        private readonly ApplicationDbContext _context = context;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+
+        public async Task InitialiseAsync()
+        {
+            try
+            {
+                if (_context.Database.IsSqlServer())
+                {
+                    await _context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while initialising the database.");
+                throw;
+            }
+        }
+        public async Task SeedAsync()
+        {
+            try
+            {
+                await TrySeedAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while seeding the database.");
+                throw;
+            }
+        }
+        public async Task TrySeedAsync()
+        {
+            // Default roles
+            var adminRole = new IdentityRole("Admin");
+
+            if (_roleManager.Roles.All(r => r.Name != adminRole.Name))
+            {
+                var role = await _roleManager.CreateAsync(adminRole);
+                if (role != null)
+                {
+                    await _roleManager.AddClaimAsync(adminRole, new Claim("RoleClaim", "HasRoleView"));
+                    await _roleManager.AddClaimAsync(adminRole, new Claim("RoleClaim", "HasRoleAdd"));
+                    await _roleManager.AddClaimAsync(adminRole, new Claim("RoleClaim", "HasRoleEdit"));
+                    await _roleManager.AddClaimAsync(adminRole, new Claim("RoleClaim", "HasRoleDelete"));
+                }
+            }
+
+            // Default users
+            var admin = new ApplicationUser { UserName = "admin", Email = "admin" };
+
+            if (_userManager.Users.All(u => u.UserName != admin.UserName))
+            {
+                await _userManager.CreateAsync(admin, "UnifiedAppAdmin1!");
+                if (!string.IsNullOrWhiteSpace(adminRole.Name))
+                {
+                    await _userManager.AddToRolesAsync(admin, new[] { adminRole.Name });
+                }
+            }
+        }
+    }
+}
