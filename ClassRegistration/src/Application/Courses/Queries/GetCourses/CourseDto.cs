3f93618cd7df7d93752979b94ad3a8c9fd3add9b
Châu Vĩnh Sinh <20520737@ms.uit.edu.vn>
2024-05-14 06:15:16 +0700

    Course, Class, Semester CRUD

---
+++ ClassRegistration/src/Application/Courses/Queries/GetCourses/CourseDto.cs
@@ -0,0 +1,17 @@
+﻿using ClassRegistration.Domain.Entities;
+
+namespace ClassRegistration.Application.Courses.Queries.GetCourses;
+public class CourseDto
+{
+    public int DepartmentId { get; set; }
+    public string? CourseCode { get; set; }
+    public string? CourseName { get; set; }
+    public string? Description { get; set; }
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Course, CourseDto>();
+        }
+    }
+}
---
+++ ClassRegistration/src/Web/Endpoints/Classes.cs
@@ -0,0 +1,42 @@
+﻿using ClassRegistration.Application.Classes.Commands.CreateClass;
+using ClassRegistration.Application.Classes.Commands.DeleteClass;
+using ClassRegistration.Application.Classes.Commands.UpdateClass;
+using ClassRegistration.Application.Classes.Queries.GetClasses;
+
+namespace ClassRegistration.Web.Endpoints;
+
+public class Classes : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapGet(GetClasses)
+            .MapPost(CreateClass)
+            .MapDelete(DeleteClass, "{id}")
+            .MapPut(UpdateClass, "{id}");
+    }
+
+    public Task<IEnumerable<ClassDto>> GetClasses(ISender sender, [AsParameters] GetClassesQuery query)
+    {
+        return sender.Send(query);
+    }
+
+    public Task<int> CreateClass(ISender sender, CreateClassCommand command)
+    {
+        return sender.Send(command);
+    }
+
+    public async Task<IResult> DeleteClass(ISender sender, int id)
+    {
+        await sender.Send(new DeleteClassCommand(id));
+        return Results.NoContent();
+    }
+
+    public async Task<IResult> UpdateClass(ISender sender, int id, UpdateClassCommand command)
+    {
+        if (id != command.Id) return Results.BadRequest();
+        await sender.Send(command);
+        return Results.NoContent();
+    }
+}
